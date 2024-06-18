82c59979bdade1e18087c9d6c367f86bd796f1ba
Petra Michael <petraxmichael@gmail.com>
2024-06-18 15:12:07 +0700

    v0.6.1 admin page css
    -updated css for the admin page

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import UsersComparison from './UsersComparison';
-
+import './UsersComparison.css';
 const App = () => {
+  useEffect(() => {
+    document.title = "Admin Panel";
+    const metaDescription = document.createElement('meta');
+    metaDescription.name = 'description';
+    metaDescription.content = 'Compare users based on some criteria';
+    document.head.appendChild(metaDescription);
+    return () => {
+      document.head.removeChild(metaDescription);
+    };
+  }, []);
+
   return (
     <div className="app">
-      <h1>Users Comparison</h1>
+      <h1>Admin Panel</h1>
       <UsersComparison />
     </div>
   );
